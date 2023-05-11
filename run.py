88392f3a801cc37e5a61ca9d681d1e0ab3efc03c
annacakes281 <>
2023-05-11 14:27:23 +0000

    add first part of text adventure using input choices

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import random
+
+
 def rules():
     """
     Rules for the game that will appear at the start so players know the goal
@@ -13,14 +16,14 @@ intro = rules()
 
 
 playing = input("Would you like to try to Escape the dungeon (yes/no)\n")
-if playing.lower() == "no":
+if playing.lower() != "yes":
     quit()
 
 player_hp = 150
 player_stamnia = 50
 
-weapon_options = ["sword", "bow and arrow"]
-master_weapon_option = ["master sword", "master bow and arrow"]
+# weapon_options = ["sword", "bow and arrow"]
+# master_weapon_option = ["master sword", "master bow and arrow"]
 master_armour = 100
 
 hp_potion_one = 0.25
@@ -39,5 +42,33 @@ master_boss_sp = 100
 name = input("Type your name:\n")
 
 print(f"Welcome {name} to the Escape the Dungeon Game. Good luck!\n")
-print(f"You start with {player_hp}hp and {player_stamnia}sp.")
-print("You also find a flimsy bow and 1 arrow, with a durablity for one use.")
+print("You have found yourself in a dungeon and must find the way out")
+print(f"You start with {player_hp}hp and {player_stamnia}sp.\n")
+
+starting_room = input(
+    "You are an empty room with nothing but dim lighting to show the way.\n"
+    "You can either go North, South or East.\n"
+    "Which way do you go? (north/south/east) \n").lower()
+
+if starting_room == "north":
+    room_one = input(
+        "You enter the room to the North and see two weapons on the ground.\n"
+        "You must decide which weapon you would like to go with.\n"
+        "There is a sword on one pedestal.\n"
+        "On the other pedestal there is a bow and a quiver full of arrows.\n"
+        "Which do you choose? (sword/bow) \n").lower()
+
+if starting_room == "south":
+    room_two = input(
+        "You enter the room to the South and come across a small lake.\n"
+        "You can see some unstable looking pedestals across the lake.\n"
+        "You can either swim throuhg the lake or jump across.\n"
+        "Which do you choose? (swim/jump) \n").lower()
+
+if starting_room == "east":
+    room_five = input(
+        "You go to the room to the East.\n"
+        "You can either go back to the previous room or go North.\n"
+        "Which way do you go? (north/west) \n").lower()
+    
+    
