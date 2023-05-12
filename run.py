febdaabd55c71f6e3a7388e764d87414a1a7d708
annacakes281 <>
2023-05-12 12:12:11 +0000

    add function for start game and edit dictionary

--- run.py
+++ run.py
@@ -8,18 +8,23 @@ player_stamina = 50
 # Map dictionary
 rooms = {
     "Starting Room": {"North": "Room 1", "South": "Room 2", "East": "Room 3"},
-    "Room 1": {"South": "Starting Room", "Key": "Key 1", "Weapon Choice": "Sword or Bow and Arrow", "Trap": "Fire"},
-    "Room 2": {"North": "Starting Room", "East": "Room 3", "Item": "Health Potion 1", "Trap": "Lake"},
+    "Room 1": {"South": "Starting Room", "Key": "Key 1", "Weapon Choice 1": "Sword", "Weapon Choice 2": "Bow", "Trap": "Fire"},
+    "Room 2": {"North": "Starting Room", "East": "Room 3", "Item": "Potion", "Trap": "Lake"},
     "Room 3": {"West": "Room 2", "North": "Room 4", "East": "Secret Room", "Mini Boss": "Imp"},
     "Room 4": {"South": "Room 3", "Key": "Key 2", "NPC": "Fairy"},
-    "Secret Room": {"West": "Room 3", "Item": "Armour", "Master Weapon": "Sword or Bow and Arrow"},
+    "Secret Room": {"West": "Room 3", "Item": "Armour", "Master Weapon 1": "Sword", "Master Weapon 2": "Bow"},
     "Room 5": {"West": "Starting Room", "North": "Room 6"},
-    "Room 6": {"South": "Room 5", "East": "Room 7", "Item": "Health Potion 2", "Trap": "Jungle"},
-    "Room 7": {"West": "Room 6", "East": "Room 9", "South": "Room 8", "Mini Boss": "Orc", "Item": "Secret Room Stone"},
+    "Room 6": {"South": "Room 5", "East": "Room 7", "Item": "Potion", "Trap": "Jungle"},
+    "Room 7": {"West": "Room 6", "East": "Room 9", "South": "Room 8", "Mini Boss": "Orc", "Item": "Stone"},
     "Room 8": {"North": "Room 7", "Item": "Secret Room Key"},
-    "Room 9": {"West": "Room 7", "South": "Boss Room", "Item": "Health Potion 3", "Key": "Master Key"}
+    "Room 9": {"West": "Room 7", "South": "Boss Room", "Item": "Potion", "Key": "Master Key"}
 }
 
+# List to keep track on invetory items
+inventory = []
+
+# Tracks the current room
+current_room = "Starting Room"
 
 def rules():
     """
@@ -51,8 +56,16 @@ def clear():
 clear()
 
 
-# Player welcome screen
+def start_game():
+    """
+    Game start intro message
+    """
+    print(f"Welcome {name} to the Escape the Dungeon Game. Good luck!\n")
+    print("You have found yourself in a dungeon and must find the way out.")
+    print(f"Current room: {current_room}")
+    print(f"Current stats: {player_hp}hp and {player_stamina}sp.")
+    print(f"Current inventory: {inventory}\n")
+
+# Player welcome screen - add as function
 name = input("Type your name:\n").strip()
-print(f"Welcome {name} to the Escape the Dungeon Game. Good luck!\n")
-print("You have found yourself in a dungeon and must find the way out.")
-print(f"You start with {player_hp}hp and {player_stamina}sp.\n")
+start_game()
